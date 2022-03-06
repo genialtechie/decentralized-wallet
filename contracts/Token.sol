334af91f81f525187c52572b5bb05fa1acab7c6e
genialtechie <thehaleembello@gmail.com>
2022-03-06 15:25:28 -0500

    created and tested token supply

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,5 +4,7 @@ pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-    constructor() ERC20("Token", "MTK") {}
+    constructor() ERC20("Token", "MTK") {
+      _mint(msg.sender, 10000);
+    }
 }
---
+++ test/Token.js
@@ -0,0 +1,20 @@
+const Token = artifacts.require('Token');
+
+let instance;
+
+//create contract instance before each test 
+beforeEach(async function (){
+    instance = await Token.deployed()
+});
+
+contract('Token Contract', async function(accounts){
+    //set up accounts
+    const deployer = accounts[0];
+    const user = accounts[1];
+
+    //test token supply
+    it('deploy', async function(){ 
+        const balance = await instance.balanceOf(deployer);
+        assert.equal(balance, 10000);
+    });
+});
